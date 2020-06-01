48911c527441188cde2a0769eb842ffd503a5be5
mwinter13 <mwinter@ECA.local>
2020-06-01 13:29:56 -0600

    board function created

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,1 +1,8 @@
 import random
+
+def drawBoard(board):
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[1] + '|' + board[2] + '|' + board[3])
