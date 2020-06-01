6283efe52b2f6c914077554d2f5e74e01e2815ad
mwinter13 <mwinter@ECA.local>
2020-06-01 14:28:04 -0600

    InputPlayerLetter defined

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -6,3 +6,18 @@ def drawBoard(board):
     print(board[4] + '|' + board[5] + '|' + board[6])
     print('-+-+-')
     print(board[1] + '|' + board[2] + '|' + board[3])
+
+def InputPlayerLetter():
+    letter = ''
+    while not (letter == 'X' or letter == 'O'):
+        #player chooses letter first
+        print('Which letter would you like to be?')
+        input().upper()
+        #now the computer gets the other letter
+    if letter == 'X':
+        return ['X', 'O'] # rationalle for two values in the returned list.  
+                            # The followoing code is found later on, 
+                            # requiring two inputs:
+                            # playerLetter, computerLetter = inputPlayerLetter()
+    else:
+        return ['O', 'X']
