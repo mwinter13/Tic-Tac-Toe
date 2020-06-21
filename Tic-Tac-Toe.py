4acf06e64371f7023b8a9676bb8af433844eafb3
mwinter13 <mwinter@ECA.local>
2020-06-21 16:43:24 -0600

    isWinnter defined

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -31,5 +31,14 @@ def whoGoesFirst():
 def makeMove(board, letter, move):
     board[move] = letter    #this changes the board list at [index] to the letter passed
 
-
-
+def isWinnter(bo, le):  #bo and le are short for board and letter
+        return
+            (bo[7] == le and bo[8] == le and bo[9] == le) or    #top
+            (bo[4] == le and bo[5] == le and bo[6] == le) or    #across middle
+            (bo[1] == le and bo[2] == le and bo[3] == le) or    #bottom
+            (bo[7] == le and bo[4] == le and bo[1] == le) or    #left
+            (bo[5] == le and bo[8] == le and bo[2] == le) or    #down middle
+            (bo[3] == le and bo[6] == le and bo[9] == le) or    #right
+            (bo[7] == le and bo[5] == le and bo[3] == le) or    #diagonal top left to bottom right
+            (bo[1] == le and bo[5] == le and bo[9] == le) or    #diagonal top right to bottom left
+            )
